62979c8fc7de14e501cb6afac78d34f3617f29d4
Vonernue <39663381+seanho12345@users.noreply.github.com>
2022-06-04 19:32:23 +0800

    Back to basic

--- cart.php
+++ cart.php
@@ -1,136 +1,134 @@
 <?php
-// Prevent direct access to file
-defined('shoppingcart') or exit;
+// If the user clicked the add to cart button on the product page we can check for the form data
+if (isset($_POST['product_id'], $_POST['quantity']) && is_numeric($_POST['product_id']) && is_numeric($_POST['quantity'])) {
+    // Set the post variables so we easily identify them, also make sure they are integer
+    $product_id = (int)$_POST['product_id'];
+    $quantity = (int)$_POST['quantity'];
+    // Prepare the SQL statement, we basically are checking if the product exists in our databaser
+    $stmt = $pdo->prepare('SELECT * FROM item WHERE item_id = ?');
+    $stmt->execute([$_POST['product_id']]);
+    // Fetch the product from the database and return the result as an Array
+    $product = $stmt->fetch(PDO::FETCH_ASSOC);
+    // Check if the product exists (array is not empty)
+    if ($product && $quantity > 0) {
+        // Product exists in database, now we can create/update the session variable for the cart
+        if (isset($_SESSION['cart']) && is_array($_SESSION['cart'])) {
+            if (array_key_exists($product_id, $_SESSION['cart'])) {
+                // Product exists in cart so just update the quanity
+                $_SESSION['cart'][$product_id] += $quantity;
+            } else {
+                // Product is not in cart so add it
+                $_SESSION['cart'][$product_id] = $quantity;
+            }
+        } else {
+            // There are no products in cart, this will add the first product to cart
+            $_SESSION['cart'] = array($product_id => $quantity);
+        }
+    }
+    // Prevent form resubmission...
+    header('location: index.php?page=cart');
+    exit;
+}
 // Remove product from cart, check for the URL param "remove", this is the product id, make sure it's a number and check if it's in the cart
 if (isset($_GET['remove']) && is_numeric($_GET['remove']) && isset($_SESSION['cart']) && isset($_SESSION['cart'][$_GET['remove']])) {
     // Remove the product from the shopping cart
-    array_splice($_SESSION['cart'], $_GET['remove'], 1);
-    header('Location: ' . url('index.php?page=cart'));
-    exit;
-}
-// Empty the cart
-if (isset($_POST['emptycart']) && isset($_SESSION['cart'])) {
-    // Remove all products from the shopping cart
-    unset($_SESSION['cart']);
-    header('Location: ' . url('index.php?page=cart'));
-    exit;
+    unset($_SESSION['cart'][$_GET['remove']]);
 }
 // Update product quantities in cart if the user clicks the "Update" button on the shopping cart page
-if ((isset($_POST['update']) || isset($_POST['checkout'])) && isset($_SESSION['cart'])) {
-    // Iterate the post data and update quantities for every product in cart
+if (isset($_POST['update']) && isset($_SESSION['cart'])) {
+    // Loop through the post data so we can update the quantities for every product in cart
     foreach ($_POST as $k => $v) {
         if (strpos($k, 'quantity') !== false && is_numeric($v)) {
             $id = str_replace('quantity-', '', $k);
-            // abs() function will prevent minus quantity and (int) will ensure the value is an integer (number)
-            $quantity = abs((int)$v);
+            $quantity = (int)$v;
             // Always do checks and validation
             if (is_numeric($id) && isset($_SESSION['cart'][$id]) && $quantity > 0) {
                 // Update new quantity
-                $_SESSION['cart'][$id]['quantity'] = $quantity;
+                $_SESSION['cart'][$id] = $quantity;
             }
         }
     }
-    // Send the user to the place order page if they click the Place Order button, also the cart should not be empty
-    if (isset($_POST['checkout']) && !empty($_SESSION['cart'])) {
-        header('Location: ' . url('index.php?page=checkout'));
-        exit;
-    }
-    header('Location: ' . url('index.php?page=cart'));
+    // Prevent form resubmission...
+    header('location: index.php?page=cart');
+    exit;
+}
+// Send the user to the place order page if they click the Place Order button, also the cart should not be empty
+if (isset($_POST['placeorder']) && isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
+    header('Location: index.php?page=placeorder');
     exit;
 }
 // Check the session variable for products in cart
-$products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
+$products_in_cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : array();
+$products = array();
 $subtotal = 0.00;
 // If there are products in cart
 if ($products_in_cart) {
     // There are products in the cart so we need to select those products from the database
-    // Products in cart array to question mark string array, we need the SQL statement to include: IN (?,?,?,...etc)
+    // Products in cart array to question mark string array, we need the SQL statement to include IN (?,?,?,...etc)
     $array_to_question_marks = implode(',', array_fill(0, count($products_in_cart), '?'));
-    // Prepare SQL statement
-    // $stmt = $pdo->prepare('SELECT p.id, pc.category_id, p.* FROM products p LEFT JOIN products_categories pc ON p.id = pc.product_id LEFT JOIN categories c ON c.id = pc.category_id WHERE p.id IN (' . $array_to_question_marks . ') GROUP BY p.id');
-    $stmt = $pdo->prepare('SELECT p.*, (SELECT m.full_path FROM products_media pm JOIN media m ON m.id = pm.media_id WHERE pm.product_id = p.id ORDER BY pm.position ASC LIMIT 1) AS img FROM products p WHERE p.id IN (' . $array_to_question_marks . ')');
-    // Leverage the array_column function to retrieve only the id's of the products
-    $stmt->execute(array_column($products_in_cart, 'id'));
+    $stmt = $pdo->prepare('SELECT * FROM item WHERE item_id IN (' . $array_to_question_marks . ')');
+    // We only need the array keys, not the values, the keys are the id's of the products
+    $stmt->execute(array_keys($products_in_cart));
     // Fetch the products from the database and return the result as an Array
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    // Iterate the products in cart and add the meta data (product name, desc, etc)
-    foreach ($products_in_cart as &$cart_product) {
-        foreach ($products as $product) {
-            if ($cart_product['id'] == $product['id']) {
-                $cart_product['meta'] = $product;
-                // Calculate the subtotal
-                $subtotal += (float)$cart_product['options_price'] * (int)$cart_product['quantity'];
-            }
-        }
+    // Calculate the subtotal
+    foreach ($products as $product) {
+        $subtotal += (float)$product['price'] * (int)$products_in_cart[$product['id']];
     }
 }
 ?>
-<?=template_header('Shopping Cart')?>
+
+<?=template_header('Cart')?>
 
 <div class="cart content-wrapper">
-
     <h1>Shopping Cart</h1>
-
-    <form action="" method="post">
+    <form action="index.php?page=cart" method="post">
         <table>
             <thead>
                 <tr>
                     <td colspan="2">Product</td>
-                    <td class="rhide"></td>
-                    <td class="rhide">Price</td>
+                    <td>Price</td>
                     <td>Quantity</td>
                     <td>Total</td>
                 </tr>
             </thead>
             <tbody>
-                <?php if (empty($products_in_cart)): ?>
+                <?php if (empty($products)): ?>
                 <tr>
-                    <td colspan="6" style="text-align:center;">You have no products added in your Shopping Cart</td>
+                    <td colspan="5" style="text-align:center;">You have no products added in your Shopping Cart</td>
                 </tr>
                 <?php else: ?>
-                <?php foreach ($products_in_cart as $num => $product): ?>
+                <?php foreach ($products as $product): ?>
                 <tr>
                     <td class="img">
-                        <?php if (!empty($product['meta']['img']) && file_exists($product['meta']['img'])): ?>
-                        <a href="<?=url('index.php?page=product&id=' . $product['id'])?>">
-                            <img src="<?=base_url?><?=$product['meta']['img']?>" width="50" height="50" alt="<?=$product['meta']['name']?>">
+                        <a href="index.php?page=product&id=<?=$product['id']?>">
+                            <img src="imgs/<?=$product['img']?>" width="50" height="50" alt="<?=$product['name']?>">
                         </a>
-                        <?php endif; ?>
                     </td>
                     <td>
-                        <a href="<?=url('index.php?page=product&id=' . $product['id'])?>"><?=$product['meta']['name']?></a>
+                        <a href="index.php?page=product&id=<?=$product['id']?>"><?=$product['name']?></a>
                         <br>
-                        <a href="<?=url('index.php?page=cart&remove=' . $num)?>" class="remove">Remove</a>
+                        <a href="index.php?page=cart&remove=<?=$product['id']?>" class="remove">Remove</a>
                     </td>
-                    <td class="options rhide">
-                        <?=htmlspecialchars(str_replace(',', ', ', $product['options']), ENT_QUOTES)?>
-                        <input type="hidden" name="options" value="<?=htmlspecialchars($product['options'], ENT_QUOTES)?>">
-                    </td>
-                    <td class="price rhide"><?=currency_code?><?=number_format($product['options_price'],2)?></td>
+                    <td class="price">&dollar;<?=$product['price']?></td>
                     <td class="quantity">
-                        <input type="number" class="ajax-update" name="quantity-<?=$num?>" value="<?=$product['quantity']?>" min="1" <?php if ($product['meta']['quantity'] != -1): ?>max="<?=$product['meta']['quantity']?>"<?php endif; ?> placeholder="Quantity" required>
+                        <input type="number" name="quantity-<?=$product['id']?>" value="<?=$products_in_cart[$product['id']]?>" min="1" max="<?=$product['quantity']?>" placeholder="Quantity" required>
                     </td>
-                    <td class="price product-total"><?=currency_code?><?=number_format($product['options_price'] * $product['quantity'],2)?></td>
+                    <td class="price">&dollar;<?=$product['price'] * $products_in_cart[$product['id']]?></td>
                 </tr>
                 <?php endforeach; ?>
                 <?php endif; ?>
             </tbody>
         </table>
-
-        <div class="total">
+        <div class="subtotal">
             <span class="text">Subtotal</span>
-            <span class="price"><?=currency_code?><?=number_format($subtotal,2)?></span>
-            <span class="note">(shipping and tax calculated at checkout)</span>
+            <span class="price">&dollar;<?=$subtotal?></span>
         </div>
-
         <div class="buttons">
-            <input type="submit" value="Update" name="update" class="btn">
-            <input type="submit" value="Empty Cart" name="emptycart" class="btn">
-            <input type="submit" value="Checkout" name="checkout" class="btn">
+            <input type="submit" value="Update" name="update">
+            <input type="submit" value="Place Order" name="placeorder">
         </div>
-
     </form>
-
 </div>
 
 <?=template_footer()?>
--- home.php
+++ home.php
@@ -1,42 +1,32 @@
 <?php
-// Prevent direct access to file
-defined('shoppingcart') or exit;
-// Get the 4 most recent added products
-$stmt = $pdo->prepare('SELECT p.*, (SELECT m.full_path FROM products_media pm JOIN media m ON m.id = pm.media_id WHERE pm.product_id = p.id ORDER BY pm.position ASC LIMIT 1) AS img FROM products p WHERE p.status = 1 ORDER BY p.date_added DESC LIMIT 4');
+// Get the 4 most recently added products
+$stmt = $pdo->prepare('SELECT * FROM item ORDER BY date_added DESC LIMIT 4');
 $stmt->execute();
 $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
 <?=template_header('Home')?>
 
-<div class="featured" style="background-image:url(<?=featured_image?>)">
-
+<div class="featured">
     <h2>Gadgets</h2>
-
     <p>Essential gadgets for everyday use</p>
-
 </div>
-
 <div class="recentlyadded content-wrapper">
-
     <h2>Recently Added Products</h2>
-
     <div class="products">
         <?php foreach ($recently_added_products as $product): ?>
-        <a href="<?=url('index.php?page=product&id=' . ($product['url_slug'] ? $product['url_slug']  : $product['id']))?>" class="product">
-            <?php if (!empty($product['img']) && file_exists($product['img'])): ?>
-            <img src="<?=$product['img']?>" width="200" height="200" alt="<?=$product['name']?>">
-            <?php endif; ?>
-            <span class="name"><?=$product['name']?></span>
+        <a href="index.php?page=product&id=<?=$product['item_id']?>" class="product">
+            <img src="imgs/<?=$product['img']?>" width="200" height="200" alt="<?=$product['item_name']?>">
+            <span class="name"><?=$product['item_name']?></span>
             <span class="price">
-                <?=currency_code?><?=number_format($product['price'],2)?>
-                <?php if ($product['rrp'] > 0): ?>
-                <span class="rrp"><?=currency_code?><?=number_format($product['rrp'],2)?></span>
+                &dollar;<?=$product['price']?>
+                <?php if ($product['discount_price'] > 0): ?>
+                <span class="rrp">&dollar;<?=$product['discount_price']?></span>
                 <?php endif; ?>
             </span>
         </a>
         <?php endforeach; ?>
     </div>
-
 </div>
 
 <?=template_footer()?>
