c1ef4afcdb7a85fb616b2884214d5301cd129f09
flin1206 <flin1206@gmail.com>
2022-06-07 19:35:04 +0800

    place order interface

--- placeorder.php
+++ placeorder.php
@@ -1,8 +1,16 @@
-<?=template_header('Place Order')?>
+<?= template_header('Place Order') ?>
 
 <div class="placeorder content-wrapper">
-    <h1>Your Order Has Been Placed</h1>
-    <p>Thank you for ordering with us, we'll contact you by email with your order details.</p>
+    <form action="" method="post">
+        <label for="Name"   class="form-label">Name</label>
+        <input type="text"  name="Name"             placeholder="Your Name" value="" required="" class="form-field">
+        <label for="credit card ID" class="form-label">Credit Card ID</label>
+        <input type="text"  name="credit card ID"   placeholder="XXXX-XXXX-XXXX-XXXX" value="" required="" class="form-field">
+        <label for="Address" class="form-label">Address</label>
+        <input type="text"  name="Address"  placeholder= "format address" value="" required="" class="form-field">
+        <br>
+        <input name="submit" type="submit" value="submit">
+    </form>
 </div>
 
-<?=template_footer()?>
+<?= template_footer() ?>
