52044670bd6ffb8ed53477ded09893ee61563eec
flin1206 <flin1206@gmail.com>
2022-06-07 20:30:18 +0800

    submit order successful interface

--- placeorder.php
+++ placeorder.php
@@ -1,15 +1,22 @@
 <?= template_header('Place Order') ?>
 
+
 <div class="placeorder content-wrapper">
-    <form action="" method="post">
-        <label for="Name"   class="form-label">Name</label>
-        <input type="text"  name="Name"             placeholder="Your Name" value="" required="" class="form-field">
-        <label for="credit card ID" class="form-label">Credit Card ID</label>
-        <input type="text"  name="credit card ID"   placeholder="XXXX-XXXX-XXXX-XXXX" value="" required="" class="form-field">
+    <form action="index.php?page=placeorder" method="post">
+        <label for="Name" class="form-label">Name</label>
+        <input type="text" name="Name" placeholder="Your Name" value="" required="" class="form-field">
         <label for="Address" class="form-label">Address</label>
-        <input type="text"  name="Address"  placeholder= "format address" value="" required="" class="form-field">
-        <br>
-        <input name="submit" type="submit" value="submit">
+        <input type="text" name="Address" placeholder="format address" value="" required="" class="form-field">
+        <label for="Phone" class="form-label">Phone</label>
+        <input type="text" name="Phone" placeholder="09XX-XXX-XXX" value="" required="" class="form-field">
+        <label for="email" class="form-label">Email</label>
+        <input type="text" name="email" placeholder="example@gmail.com" value="" required="" class="form-field">
+        <br><br>
+        <div class="buttons">
+            <a href="index.php?page=submitorder">
+                submit
+            </a>
+        </div>
     </form>
 </div>
 
---
+++ submitorder.php
@@ -0,0 +1,10 @@
+<?= template_header('') ?>
+
+<div class="placeorder content-wrapper">
+    <form action="" method="post">
+        <p>Thank you for your Patronage!</p>
+        <p>Your order will be shipped immediately.</p>
+    </form>
+</div>
+
+<?= template_footer() ?>
